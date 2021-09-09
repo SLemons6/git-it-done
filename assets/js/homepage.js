5b9bf5ecb6970448e9c6676e9a1814ab17fd1936
Shayne Lemons <lemonsshayne6@gmail.com>
2021-09-09 08:47:50 -0700

    add API response data and error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,25 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    // prevent page from refreshing
+    event.preventDefault();
+
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+
+        // clear old content 
+        repoContainerEl.textContent = '';
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -23,35 +43,15 @@ var getUserRepos = function(user) {
         });
 };
 
-var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#username");
-
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-    // get value from input element
-    var username = nameInputEl.value.trim();
-
-    if (username) {
-        getUserRepos(username);
-        nameInputEl.value = "";
-    } else {
-        alert("Please enter a GitHub username");
-    }
-
-    console.log(event);
-};
-
 var displayRepos = function(repos, searchTerm) {
     // check if api returned any repos
     if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found.";
         return;
     }
-    // clear old content
-    repoContainerEl.textContent = "";
+
     repoSearchTerm.textContent = searchTerm;
-    console.log(repos);
-    console.log(searchTerm);
+
     // loop over repos
     for (var i = 0; i < repos.length; i++) {
         // format repo name
@@ -82,9 +82,11 @@ var displayRepos = function(repos, searchTerm) {
 
         // append to container
         repoEl.appendChild(statusEl);
+
         // append container to the dom
         repoContainerEl.appendChild(repoEl);
     }
 };
 
+// add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
