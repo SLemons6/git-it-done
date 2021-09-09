8b1407feb60e599fe29969ddd210861d3588df30
Shayne Lemons <lemonsshayne6@gmail.com>
2021-09-09 10:04:48 -0700

    add function to link index.html to single-repo.html

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,7 @@
-var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var formSubmitHandler = function(event) {
     // prevent page from refreshing
@@ -38,7 +38,6 @@ var getUserRepos = function(user) {
             }
         })
         .catch(function(error) {
-            // Notice this `.catch()` getting chained onto the end of the `.then()` method
             alert("Unable to connect to GitHub");
         });
 };
@@ -58,8 +57,9 @@ var displayRepos = function(repos, searchTerm) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
         // create a container for each repo
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute = ("href", "./single-repo.html");
 
         // create a span element to hold repository name
         var titleEl = document.createElement("span");
